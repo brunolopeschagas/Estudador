ba7adcef9a634dd5addbbf3d4c58c6447e836738
Bruno <bruno.chagas@gmail.com>
2021-06-12 16:19:05 -0300

    launcher alterado

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -6,6 +6,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Estudador',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
